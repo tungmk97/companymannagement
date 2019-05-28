5896f8da65840b4f6a257158b8b4ee6fc5dcdb8b
tungmk97 <tungnv975@gmail.com>
2019-05-28 11:05:13 +0700

    add i18n

---
+++ src/main/java/com/mk/company/config/i18nConfig.java
@@ -0,0 +1,18 @@
+package com.mk.company.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class i18nConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setCacheSeconds(1800);
+
+        return messageSource;
+    }
+}
