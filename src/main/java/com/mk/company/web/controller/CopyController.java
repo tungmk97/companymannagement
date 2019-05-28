58100ed37bfaae5c97c37c14b339854a99457d41
tungmk97 <tungnv975@gmail.com>
2019-05-28 11:43:15 +0700

    add about page

---
+++ src/main/java/com/mk/company/web/controller/CopyController.java
@@ -0,0 +1,15 @@
+package com.mk.company.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * create by TungNguyen on May, 2019
+ */
+@Controller
+public class CopyController {
+    @RequestMapping(value = "/about")
+    public String about() {
+        return "copy/about";
+    }
+}
